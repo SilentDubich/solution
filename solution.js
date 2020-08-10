d031ea8be9fa89f9270f8c2de6c740aeed526a91
SilentDubich <kirill.dubov.2012@mail.ru>
2020-08-10 12:58:02 +0300

    Оптимизирован алгоритм

--- solution.js
+++ solution.js
@@ -6,7 +6,7 @@ const arr = [
     { branch: 'ford', model: 'explorer', year: '2020' },
     { branch: 'bmw', model: 'x7', year: '2020' },
 ];
-const groupType = 'model'
+const groupType = 'branch'
 
 const groupBy = (arr, groupType) => {
     //прошу прощения за столь грубое решение, ночь без сна из за кашля несколько подпортила решение (
@@ -16,23 +16,29 @@ const groupBy = (arr, groupType) => {
     })
     let result = {}
     let group = []
+    // Выделяем основные группы
     for (let i = 0; i < arr.length; i++) {
         if (group.indexOf(arr[i][groupType]) === -1) {
             group.push(arr[i][groupType])
         }
     }
+    //Создаем для этих групп свойства в объекте и присваиваем им пустой массив
     for (let j = 0; j < group.length; j++) {
             let property = group[j]
             result[property] = []
     }
-    // За это отдельно прошу прощения, такой кошмар я обычно не делаю
+    //Заполняем эти массивы нужными данными
     for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < group.length; j++) {
-            if (arr[i][groupType] === group[j]) {
-                result[group[j]].push(arr[i])
-            }
-        }
+        result[arr[i][groupType]].push(arr[i])
     }
+    // За это отдельно прошу прощения, такой кошмар я обычно не делаю
+    // for (let i = 0; i < arr.length; i++) {
+    //     for (let j = 0; j < group.length; j++) {
+    //         if (arr[i][groupType] === group[j]) {
+    //             result[group[j]].push(arr[i])
+    //         }
+    //     }
+    // }
     console.log(result)
     return result
 }
